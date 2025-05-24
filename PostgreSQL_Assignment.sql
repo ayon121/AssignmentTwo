8f4c7652bad4db365ca0d361a03f680ddfc4f008
ayon121 <borshonsaha1234@gmail.com>
2025-05-25 02:11:59 +0600

    first commit

---
+++ PostgreSQL_Assignment.sql
@@ -0,0 +1,29 @@
+-- Creating rangers table
+-- Fields : ranger_id , name , region
+
+
+
+
+
+
+
+
+
+
+-- Creating species table
+-- Fields : species_id , common_name , scientific_name , discovery_date , conservation_status
+
+
+
+
+
+
+
+
+
+
+--Creating sightings table
+-- Fields : sighting_id , ranger_id , species_id , sighting_time , location , notes
+
+
+
